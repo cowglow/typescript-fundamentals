0598fce4ad508290b0b1fd4c2e4b3f255ae6d974
Philip Saa <cowglow@gmail.com>
2020-05-13 01:00:57 +0200

    Update project webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
 const path = require("path");
 
+const dest = "public/script";
+
 module.exports = {
+  watch: true,
   entry: {
     app: ["./src/app.ts"]
   },
   mode: "development",
   module: {
-    rules: [{ test: /\.ts$/, use: "ts-loader" }]
+    rules: [
+      { test: /\.ts$/, use: "ts-loader" },
+    ]
   },
   resolve: {
     extensions: [".ts"]
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, dest),
     filename: "app.js"
   },
   devServer: {
-    contentBase: path.resolve(__dirname, "public"),
+    contentBase: path.resolve(__dirname, dest),
     compress: true,
     hot: true,
     port: 9000,
