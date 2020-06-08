b7ee3a740310823975f2228c25a86955c381687d
Philip Saa <cowglow@gmail.com>
2020-06-08 12:49:34 +0200

    Add random hash symbol
    Signed-off-by: Philip Saa <cowglow@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,25 @@
  * License: MIT
  */
 
+import { ValidSymbol } from "./domain/types/valid-symbol";
 import { GetRandomHash } from "./application/get-random-hash";
 
-const targetElement = document.getElementById("app");
+const symbolCollection: ValidSymbol[] = ["#", "$"];
+const length: number = 7;
 
+const hashSymbol = (symbolMap): ValidSymbol => {
+  return symbolMap[Date.now() % 2];
+};
+
+/** Display it */
+const targetElement = document.getElementById("app");
 if (targetElement) {
-  targetElement.innerText = GetRandomHash("#", 7);;
+  targetElement.innerText = GetRandomHash("#", length);
 
   setInterval(() => {
-    targetElement.innerText = GetRandomHash("$", 7);
+    targetElement.innerText = GetRandomHash(
+      hashSymbol(symbolCollection),
+      length
+    );
   }, 1000);
 }
